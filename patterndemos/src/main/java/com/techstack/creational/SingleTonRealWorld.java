b3525f1b45d8903d5e6dffdd1d23714c1f248004
tulasiprasad <prasad.nadupuri@gmail.com>
2018-08-21 22:00:32 +0530

    updated comments

--- patterndemos/src/main/java/com/techstack/creational/SingleTonRealWorld.java
+++ patterndemos/src/main/java/com/techstack/creational/SingleTonRealWorld.java
@@ -1,15 +1,25 @@
-// SingleTon in realTime usuage 
-
+// SingleTon in realTime usage 
+ 
 package com.techstack.creational;
 
 /**
  * 
+ * The main objective of this code is to show at any given point we could see only one Runtime instance .
+ * 
  * @author tulasiprasad
+ * @version 1.0
  *
  */
+
 public class SingleTonRealWorld {
 
+	/**
+	 * The main method.
+	 *
+	 * @param args the arguments
+	 */
 	public static void main(String... args) {
+		
 		Runtime runtime = Runtime.getRuntime();
 		runtime.gc();
 		System.out.println(runtime);
