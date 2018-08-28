b561ee92dc303a4a0c861bc875ea91e2ad66f0b7
Prasad <prasadT@emea.travelex.net>
2018-08-28 18:58:42 +0530

    added springDockersample project

---
+++ springDockerSample/src/main/java/com/techstack/SpringDockerSampleApplication.java
@@ -0,0 +1,12 @@
+package com.techstack;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SpringDockerSampleApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SpringDockerSampleApplication.class, args);
+	}
+}
