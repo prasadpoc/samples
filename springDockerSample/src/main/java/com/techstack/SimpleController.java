258b12621f8162c024b0717f061a9d1b5b082d34
Prasad <prasadT@emea.travelex.net>
2018-08-28 19:11:37 +0530

    Added controller for testing rest

---
+++ springDockerSample/src/main/java/com/techstack/SimpleController.java
@@ -0,0 +1,25 @@
+package com.techstack;
+
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+/**
+ * The Class SimpleController.
+ * 
+ * @author prasadt
+ * @version 1.0
+ */
+@RestController
+public class SimpleController {
+
+	/**
+	 * Docker test.
+	 *
+	 * @return the string
+	 */
+	@GetMapping("/dockerTest")
+	public String dockerTest() {
+		return ("success");
+	}
+
+}
--- springDockerSample/src/main/java/com/techstack/SpringDockerSampleApplication.java
+++ springDockerSample/src/main/java/com/techstack/SpringDockerSampleApplication.java
@@ -3,9 +3,21 @@ package com.techstack;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
+/**
+ * The Class SpringDockerSampleApplication.
+ *
+ * @author prasadt
+ * @version 1.0
+ */
+
 @SpringBootApplication
 public class SpringDockerSampleApplication {
 
+	/**
+	 * The main method.
+	 *
+	 * @param args the arguments
+	 */
 	public static void main(String[] args) {
 		SpringApplication.run(SpringDockerSampleApplication.class, args);
 	}
