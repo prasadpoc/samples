ffed442cb7603a23689ea64bb4853a5cbdc4e414
Prasad <prasadT@emea.travelex.net>
2018-08-29 17:10:13 +0530

    updated for Eager and LasyLoading objects

--- patterndemos/src/main/java/com/techstack/Demo.java
+++
@@ -1,8 +0,0 @@
-package com.techstack;
-
-public class Demo {
-
-	public static void main(String... args) {
-		System.out.println("test");
-	}
-}
--- patterndemos/src/main/java/com/techstack/creational/singleTon/Test.java
+++ patterndemos/src/main/java/com/techstack/creational/singleTon/Test.java
@@ -10,11 +10,19 @@ public class Test {
 	public static void main(String... args) {
 
 		// both Sys outs print single database.
-		DatabaseSingleTon databaseInstance1 = DatabaseSingleTon.getInstance();
+		DatabaseSingleTonEagerLoad databaseInstance1 = DatabaseSingleTonEagerLoad.getInstance();
 		System.out.println(databaseInstance1);
 
-		DatabaseSingleTon databaseInstance2 = DatabaseSingleTon.getInstance();
+		DatabaseSingleTonEagerLoad databaseInstance2 = DatabaseSingleTonEagerLoad.getInstance();
 		System.out.println(databaseInstance2);
+		
+		
+		// both Sys outs print single database.
+		DatabaseSingleTonLasyLoad databaseInstance3 = DatabaseSingleTonLasyLoad.getInstance();
+		System.out.println(databaseInstance3);
+
+		DatabaseSingleTonLasyLoad databaseInstance4 = DatabaseSingleTonLasyLoad.getInstance();
+		System.out.println(databaseInstance4);
 		
 	}
 }
